65840bb0ba23f289d46c7d1b3d2c1d6ce29c6398
tuongvy2679 <70678490+tuongvy2679@users.noreply.github.com>
2020-10-15 16:38:39 +0700

    update

--- src/app/main/product/product/product.component.ts
+++ src/app/main/product/product/product.component.ts
@@ -1,7 +1,7 @@
 import { MustMatch } from '../../../helpers/must-match.validator';
 import { Component, Injector, OnInit, ViewChild } from '@angular/core';
 import { FileUpload } from 'primeng/fileupload';
-import { FormBuilder, Validators} from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { BaseComponent } from '../../../lib/base-component';
 import 'rxjs/add/operator/takeUntil';
 declare var $: any;
@@ -14,15 +14,15 @@ declare var $: any;
 export class ProductComponent extends BaseComponent implements OnInit {
   public items: any;
   public item: any;
-  public totalRecords:any;
+  public totalRecords: any;
   public pageSize = 3;
   public page = 1;
   public uploadedFiles: any[] = [];
   public formsearch: any;
   public formdata: any;
-  public doneSetupForm: any;  
-  public showUpdateModal:any;
-  public isCreate:any;
+  public doneSetupForm: any;
+  public showUpdateModal: any;
+  public isCreate: any;
   submitted = false;
   @ViewChild(FileUpload, { static: false }) file_image: FileUpload;
   constructor(private fb: FormBuilder, injector: Injector) {
@@ -31,35 +31,34 @@ export class ProductComponent extends BaseComponent implements OnInit {
 
   ngOnInit(): void {
     this.formsearch = this.fb.group({
-      'hoten': [''],
-      'taikhoan': [''],     
+      'item_name': [''],
     });
-   this.search();
+    this.search();
   }
 
-  loadPage(page) { 
-    this._api.post('/api/items/search',{page: page, pageSize: this.pageSize}).takeUntil(this.unsubscribe).subscribe(res => {
+  loadPage(page) {
+    this._api.post('/api/item/search', { page: page, pageSize: this.pageSize }).takeUntil(this.unsubscribe).subscribe(res => {
       this.items = res.data;
-      this.totalRecords =  res.totalItems;
+      this.totalRecords = res.totalItems;
       this.pageSize = res.pageSize;
-      });
-  } 
+    });
+  }
 
-  search() { 
+  search() {
     this.page = 1;
     this.pageSize = 5;
-    this._api.post('/api/items/search',{page: this.page, pageSize: this.pageSize, hoten: this.formsearch.get('hoten').value, taikhoan: this.formsearch.get('taikhoan').value}).takeUntil(this.unsubscribe).subscribe(res => {
+    this._api.post('/api/item/search', { page: this.page, pageSize: this.pageSize, hoten: this.formsearch.get('item_name').value }).takeUntil(this.unsubscribe).subscribe(res => {
       this.items = res.data;
-      this.totalRecords =  res.totalItems;
+      this.totalRecords = res.totalItems;
       this.pageSize = res.pageSize;
-      });
+    });
   }
 
-  pwdCheckValidator(control){
-    var filteredStrings = {search:control.value, select:'@#!$%&*'}
+  pwdCheckValidator(control) {
+    var filteredStrings = { search: control.value, select: '@#!$%&*' }
     var result = (filteredStrings.select.match(new RegExp('[' + filteredStrings.search + ']', 'g')) || []).join('');
-    if(control.value.length < 6 || !result){
-        return {matkhau: true};
+    if (control.value.length < 6 || !result) {
+      return { matkhau: true };
     }
   }
 
@@ -69,72 +68,53 @@ export class ProductComponent extends BaseComponent implements OnInit {
     this.submitted = true;
     if (this.formdata.invalid) {
       return;
-    } 
-    if(this.isCreate) { 
+    }
+    if (this.isCreate) {
       this.getEncodeFromImage(this.file_image).subscribe((data: any): void => {
         let data_image = data == '' ? null : data;
         let tmp = {
-           image_url:data_image,
-           hoten:value.hoten,
-           diachi:value.diachi,
-           gioitinh:value.gioitinh,
-           email:value.email,
-           taikhoan:value.taikhoan,
-           matkhau:value.matkhau,
-           role:value.role,
-           ngaysinh:value.ngaysinh           
-          };
-        this._api.post('/api/items/create-item',tmp).takeUntil(this.unsubscribe).subscribe(res => {
+          item_image: data_image,
+          item_name: value.item_name,
+          item_price: value.item_price,
+          item_description: value.item_description
+        };
+        this._api.post('/api/item/create-item', tmp).takeUntil(this.unsubscribe).subscribe(res => {
           alert('Thêm thành công');
           this.search();
           this.closeModal();
-          });
+        });
       });
-    } else { 
+    } else {
       this.getEncodeFromImage(this.file_image).subscribe((data: any): void => {
         let data_image = data == '' ? null : data;
         let tmp = {
-           image_url:data_image,
-           hoten:value.hoten,
-           diachi:value.diachi,
-           gioitinh:value.gioitinh,
-           email:value.email,
-           taikhoan:value.taikhoan,
-           matkhau:value.matkhau,
-           role:value.role,
-           ngaysinh:value.ngaysinh ,
-           item_id:this.item.item_id,          
-          };
-        this._api.post('/api/items/update-item',tmp).takeUntil(this.unsubscribe).subscribe(res => {
+          item_image: data_image,
+          item_name: value.item_name,
+          item_price: value.item_price,
+          item_description: value.item_description
+        };
+        this._api.post('/api/item/update-item', tmp).takeUntil(this.unsubscribe).subscribe(res => {
           alert('Cập nhật thành công');
           this.search();
           this.closeModal();
-          });
+        });
       });
     }
-  } 
-  onDelete(row) { 
-    this._api.post('/api/items/delete-item',{item_id:row.item_id}).takeUntil(this.unsubscribe).subscribe(res => {
+  }
+  onDelete(row) {
+    this._api.post('/api/item/delete-item', { item_id: row.item_id }).takeUntil(this.unsubscribe).subscribe(res => {
       alert('Xóa thành công');
-      this.search(); 
-      });
+      this.search();
+    });
   }
 
-   Reset() {  
+  Reset() {
     this.item = null;
     this.formdata = this.fb.group({
-      'hoten': ['', Validators.required],
-      'ngaysinh': [this.today, Validators.required], 
-      'diachi': [''],
-      'gioitinh': [this.genders[0].value, Validators.required],
-      'email': ['', [Validators.required,Validators.email]],
-      'taikhoan': ['', Validators.required],
-      'matkhau': ['', [this.pwdCheckValidator]],
-      'nhaplaimatkhau': ['', Validators.required],
-      'role': [this.roles[0].value, Validators.required],
-    }, {
-      validator: MustMatch('matkhau', 'nhaplaimatkhau')
-    }); 
+      'item_name': ['', Validators.required],
+      'item_price': ['', Validators.required],
+      'item_description': [''],
+    });
   }
 
   createModal() {
@@ -145,49 +125,30 @@ export class ProductComponent extends BaseComponent implements OnInit {
     setTimeout(() => {
       $('#createitemModal').modal('toggle');
       this.formdata = this.fb.group({
-        'hoten': ['', Validators.required],
-        'ngaysinh': ['', Validators.required],
-        'diachi': [''],
-        'gioitinh': ['', Validators.required],
-        'email': ['', [Validators.required,Validators.email]],
-        'taikhoan': ['', Validators.required],
-        'matkhau': ['', [this.pwdCheckValidator]],
-        'nhaplaimatkhau': ['', Validators.required],
-        'role': ['', Validators.required],
-      }, {
-        validator: MustMatch('matkhau', 'nhaplaimatkhau')
+        'item_name': ['', Validators.required],
+        'item_price': ['', Validators.required],
+        'item_description': [''],
       });
-      this.formdata.get('ngaysinh').setValue(this.today);
-      this.formdata.get('gioitinh').setValue(this.genders[0].value); 
-      this.formdata.get('role').setValue(this.roles[0].value);
+
       this.doneSetupForm = true;
     });
   }
-  
+
   public openUpdateModal(row) {
     this.doneSetupForm = false;
-    this.showUpdateModal = true; 
+    this.showUpdateModal = true;
     this.isCreate = false;
     setTimeout(() => {
       $('#createitemModal').modal('toggle');
-      this._api.get('/api/items/get-by-id/'+ row.item_id).takeUntil(this.unsubscribe).subscribe((res:any) => {
-        this.item = res; 
-        let ngaysinh = new Date(this.item.ngaysinh);
-          this.formdata = this.fb.group({
-            'hoten': [this.item.hoten, Validators.required],
-            'ngaysinh': [ngaysinh, Validators.required],
-            'diachi': [this.item.diachi],
-            'gioitinh': [this.item.gioitinh, Validators.required],
-            'email': [this.item.email, [Validators.required,Validators.email]],
-            'taikhoan': [this.item.taikhoan, Validators.required],
-            'matkhau': [this.item.matkhau, [this.pwdCheckValidator]],
-            'nhaplaimatkhau': [this.item.matkhau, Validators.required],
-            'role': [this.item.role, Validators.required],
-          }, {
-            validator: MustMatch('matkhau', 'nhaplaimatkhau')
-          }); 
-          this.doneSetupForm = true;
-        }); 
+      this._api.get('/api/item/get-by-id/' + row.item_id).takeUntil(this.unsubscribe).subscribe((res: any) => {
+        this.item = res;
+        this.formdata = this.fb.group({
+          'item_name': ['', Validators.required],
+          'item_price': ['', Validators.required],
+          'item_description': [''],
+        },);
+        this.doneSetupForm = true;
+      });
     }, 700);
   }
   closeModal() {
